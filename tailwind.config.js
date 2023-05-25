3b01933fc6a108135c21e889383bb451ad7a45e4
Koshice <khoa93.pch@gmail.com>
2023-05-25 22:51:04 +0700

    Fix NavBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", 
+  content: ["./public/**/*.{html,js}", 
             "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
